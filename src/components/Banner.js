709be1ce2d117daf5caa454831a604e6b36dec76
kyungbin <pkb8839@naver.com>
2022-11-28 21:24:48 +0900

    [banner 만들기]

---
+++ src/components/Banner.js
@@ -0,0 +1,48 @@
+import axios from '../api/axios';
+import React, { useEffect, useState } from 'react';
+import requests from "../api/request";
+
+export default function Banner() {
+    const [movie, setMoive] = useState([]);
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        // 현재 상영중인 영화 정보를 가져오기(여러 영화)
+        const request = await axios.get(requests.fetchNowPlaying);
+        console.log(request);
+        // 여러 영화 중 영화 하나의 ID를 가져오기 
+        const movieId = request.data.results[Math.floor(Math.random() * request.data.results.length)].id;
+
+        // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
+        const {data: movieDetail} = await axios.get(`movie/${movieId}`, {
+            params: {append_to_reponse : "videos" },
+        });
+        setMoive(movieDetail);
+    };
+  return (
+    <header 
+        className='banner'
+        style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+            backgroundPosition: "top center",
+            backgroundSize: "cover"
+        }}>
+        <div className='banner__contens'>
+            <h1>
+                {movie.title || movie.name || movie.original_name}
+            </h1>
+            <div className='banner__buttons'>
+                <button
+                    className='banner__button play'>
+                        Play
+                    </button>
+                    <button className='banner__button info'>More Information</button>
+            </div>
+            <h1 className='banner_description'>{movie.overview}</h1>
+        </div>
+        <div className='banner--fadeBottom'></div>
+    </header>
+  );
+};
