9d914fdd45f7c7a3e0bdd79d1abfe274a95376a7
kyungbin <pkb8839@naver.com>
2023-01-17 16:54:15 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
+import requests from './api/request';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
       <Banner />
+      <Row 
+        title="NETFLIX ORIGINAILS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Romance Movies" id="RM" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,14 +19,14 @@ function Nav() {
   return (
     <nav className={`nav ${show && "nav__black"}`}>
         <img
-            alt='Netflix logo'
-            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-            className="nav__logo"
+              alt="Netflix logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+              className="nav__logo"
             onClick={() => window.location.reload()}
         />
         <img 
-            alt="User logged"
-            src="https://ko.wikipedia.org/wiki/%EC%9C%84%ED%82%A4%EB%B0%B1%EA%B3%BC:%EC%9C%A0%EC%9A%A9%ED%95%9C_%EC%95%84%EC%9D%B4%EC%BD%98_%EB%AA%A8%EC%9D%8C#/media/%ED%8C%8C%EC%9D%BC:User_icon_2.svg"
+        alt="User logged"
+        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
             className="nav__avatar"
         />
     </nav>
