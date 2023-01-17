82f9bf6626b5495c3cd5b6c9bb1793656f05eeb5
kyungbin <pkb8839@naver.com>
2023-01-17 17:29:33 +0900

    Styled Component를 이용해서 Footer 생성하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './components/Banner';
 import Nav from './components/Nav';
 import Row from './components/Row';
 import requests from './api/request';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
       <Row title="Romance Movies" id="RM" fetchUrl={requests.fetchRomanceMovies} />
+      <Footer />
     </div>
   );
 }
