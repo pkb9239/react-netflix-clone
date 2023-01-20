705a5cee83ab4bb9ca8c7864a9d9c92eeb62068b
kyungbin <pkb8839@naver.com>
2023-01-20 14:54:29 +0900

    검색 페이지 구현하기

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from "../../api/axios";
+import "./SearchPage.css";
 
 function SearchPage() {
     const [searchResults, setSearchResults] = useState([]);
@@ -28,10 +29,42 @@ function SearchPage() {
         console.log("error");
       }
     }
-  return (
-    <div>
-    </div>
-  )
+
+    const renderSearchResults = () => {
+      return searchResults.length > 0 ? (
+        <section className='search-container'>
+          {searchResults.map((movie) => {
+            if (movie.backdrop_path !== null && movie.media_type !== "person") {
+              const movieImageUrl = 
+                "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
+                return (
+                  <div className='movie'>
+                    <div
+                      className='movie__column-poster'
+                    >
+                      <img
+                        src={movieImageUrl} 
+                        alt="movie"
+                        className='movie__poster'
+                        />
+                    </div>
+                  </div>
+                )
+            }
+          })}
+        </section>
+      ) 
+      : (
+        <section className='no-results'>
+          <div className='no-results__text'>
+            <p>
+              찾고자하는 검색어 "{searchTerm}" 에 맞는 영화가 없습니다.
+            </p>
+          </div>
+        </section>
+      )
+    }
+  return renderSearchResults();
 }
 
 export default SearchPage
