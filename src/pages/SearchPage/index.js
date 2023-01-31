2573853d1a21e1c62c04dc1179f042ba43f98288
kyungbin <pkb8839@naver.com>
2023-01-31 12:10:48 +0900

    useParms를 이용한 영화 상세 페이지 구현하기

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from "../../api/axios";
 import useDebounce from '../../hooks/useDebounce';
 import "./SearchPage.css";
 
 function SearchPage() {
+    const navigate = useNavigate();
+
     const [searchResults, setSearchResults] = useState([]);
 
     const useQuery = () => {
@@ -41,9 +43,7 @@ function SearchPage() {
                 "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
                 return (
                   <div className='movie' key={movie.id}>
-                    <div
-                      className='movie__column-poster'
-                    >
+                    <div onClick={() => navigate(`/${movie.id}`)} className='movie__column-poster'>
                       <img
                         src={movieImageUrl} 
                         alt="movie"
